3a182169b51d53cb17dc3294bdf0a32f1d9dcdaf
KonstantinMihaylov <72294158+KonstantinMihaylov@users.noreply.github.com>
2020-10-17 09:30:14 +0700

    дороботка

--- Лабораторная работа 1.1/Source.cpp
+++ Лабораторная работа 1.1/Source.cpp
@@ -1,40 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
-{
-    for (int i = 1; i <= 100; i++)
+{ 
+  for (int i = 1; i <= 100; i++)
+  {
+    if ((i % 3 == 0) && (i % 5 != 0))
     {
-
-        if (i % 5 == 0 && i % 3 == 0)
-        {
-            printf("FizzBuzz\n");
-        }
-        else
-        {
-            if (i % 5 == 0)
-            {
-                printf("Buzz\n");
-            }
-            else
-            {
-                if (i % 3 == 0)
-                {
-                    printf("Fizz\n");
-                }
-                else
-                {
-                    
-                    printf("%d\n", i);
-                  
-
-                }
-            }
-        }
-
-       
-
-       
+      printf("Fizz\n");
     }
-
-    return 0;
+    if ((i % 3 != 0) && (i % 5 == 0))
+    {
+      printf("Buzz\n");
+    }
+    if ((i % 3 == 0) && (i % 5 == 0))
+    {
+      printf("FizzBuzz\n");
+    }
+    if ((i % 3 != 0) && (i % 5 != 0))
+    {
+      printf("%d\n", i);
+    }
+  }
+  
+  return 0;
 }
